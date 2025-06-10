f6beca6d3ecbe8df3b7eea8e82a2bf32ce863ad0
yogendrarau <yogendra.rautela7@gmail.com>
2025-06-10 03:22:21 -0400

    sdfasdff

--- db.js
+++ db.js
@@ -1,14 +1,3 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
-// Initialize database tables
 async function initDb() {
   try {
     await pool.query(`
